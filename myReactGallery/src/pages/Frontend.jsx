55240c2745a705f01e884ba093f30538d5bdf6fe
Rasmus Krogsgaard <113165532+rasmuskrogsgaard@users.noreply.github.com>
2024-01-24 17:23:19 +0100

    Initial commit

---
+++ myReactGallery/src/pages/Frontend.jsx
@@ -0,0 +1,14 @@
+
+export const FrontPage = () => {
+    return (
+        <>
+            <div class="grid grid-cols-3 grid-rows-3"></div>
+            <div><img src="../src/assets/galleryImageOne.jpg" alt="art"></img></div>
+
+
+        </>
+
+    )
+
+
+}
